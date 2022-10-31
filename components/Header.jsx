f435fccf6ab2b3d64d9996aebc604d87819552c0
rouane-mouaad <mouaadrio@gmail.com>
2022-10-31 18:46:45 -0400

    search bar added

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../public/logo.svg'
 import mobileLogo from '../public/Ulaval_mobile.svg'
+import { MagnifyingGlassIcon  } from '@heroicons/react/24/outline'
+
 
 export default function Header() {
   return (
-    <div>
 
-        {/*Logo*/}
-        <div className='flex items-center justify-between max-w-6xl'>
+      <div className='flex items-center justify-between max-w-6xl'>
+            {/*Logo*/}
             <div className='h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
                 <Image
                     src={logo}
@@ -24,14 +25,16 @@ export default function Header() {
                     className='object-contain'
                 />
             </div>
+            
+        {/*Search bar*/}
+        <div className='relative mt-1'>
+            <div className='absolute left-2 top-2'>
+                <MagnifyingGlassIcon  className='h-5 text-gray-500 '/>
+            </div>
+            <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md '/>
+        </div>
+        {/*profil image*/}
             <h1>Profile</h1>
         </div>
-
-        {/*Search bar*/}
-        {/*profil image*/}
-
-
-
-    </div>
-  )
+  );
 }
