762100d5f261c3fe176ab9f5850ec31719dcf84e
rouane-mouaad <mouaadrio@gmail.com>
2022-10-31 21:17:50 -0400

    right side menu, and profile image were added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
-import logo from '../public/logo.svg'
+import logo from '../public/logo_ulaval.png'
 import mobileLogo from '../public/Ulaval_mobile.svg'
-import { MagnifyingGlassIcon  } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon, PlusCircleIcon} from '@heroicons/react/24/outline'
+import {HomeIcon} from '@heroicons/react/24/solid'
 
 
 export default function Header() {
   return (
 
-      <div className='flex items-center justify-between max-w-6xl'>
+      <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
             {/*Logo*/}
-            <div className='h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
+            <div className='items-center cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
                 <Image
                     src={logo}
                     layout= 'fill'
@@ -34,7 +35,11 @@ export default function Header() {
             <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md '/>
         </div>
         {/*profil image*/}
-            <h1>Profile</h1>
+            <div className="flex space-x-4 items-center">
+                <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <img src='https://avatars.githubusercontent.com/u/78082985?s=400&u=bca0e4d2c7282218a9f41d808fd85d7f1a5eb8e4&v=4' alt='user_image' className='rounded-full h-10 cursor-pointer' />
+            </div>
         </div>
   );
 }
