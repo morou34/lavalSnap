ad1c371747be06d736a2f4ce77e2b877b4ca3555
rouane-mouaad <mouaadrio@gmail.com>
2022-11-01 21:42:49 -0400

    Right section and mini profile is done

---
+++ components/MiniProfile.jsx
@@ -0,0 +1,12 @@
+export default function MiniProfile() {
+  return (
+    <div className="flex items-center justify-between mt-8 ml-4">
+        <img src='https://avatars.githubusercontent.com/u/78082985?s=400&u=bca0e4d2c7282218a9f41d808fd85d7f1a5eb8e4&v=4' alt='user_image' className='rounded-full h-16 cursor-pointer border p-[2px]' />
+        <div className="flex-1 ml-4">
+            <h2 className="font-bold">ROUANE Mouaad</h2>
+            <h3 className="text-sm text-gray-400">Welcome to LavalSquare</h3>
+        </div>
+        <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+    </div>
+  )
+}
--- components/Post.jsx
+++ components/Post.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { EllipsisHorizontalIcon} from '@heroicons/react/24/outline'
+import { 
+  EllipsisHorizontalIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  FaceSmileIcon,
+  BookmarkIcon, 
+  HeartIcon,
+} from '@heroicons/react/24/outline'
 
 
 export default function Post({img, userImg, caption, username, id}) {
@@ -16,6 +22,31 @@ export default function Post({img, userImg, caption, username, id}) {
 
       {/* Post Image */}
       <img className='object-cover w-full' src={img} alt="" />
+
+      {/* Post Likes And Comments */}
+      <div className='flex justify-between px-4 pt-4'>
+        <div className='flex space-x-4'>
+          <HeartIcon className='post_btn'/>
+          <ChatBubbleOvalLeftEllipsisIcon className='post_btn'/>
+        </div>
+        <BookmarkIcon className='post_btn' />
+      </div>
+
+      {/* Post Comments  */}
+      <p className='p-5 truncate'>
+        <span className='font-bold mr-2'>
+          {username}
+        </span>
+          {caption}
+      </p>
+
+      {/* Post Input Box */}
+      <form className='flex items-center p-4'>
+        <FaceSmileIcon className='post_btn h-7'/>
+        <input className='border-none flex-1 focus:ring-0' type="text" placeholder='Enter your comment ...' />
+        <button className='text-blue-400 font-bold'>Publish</button>
+      </form>
+
     </div>
   )
 }
